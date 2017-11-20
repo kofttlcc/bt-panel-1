d8371b54af026a96e2dfa7b835ae0b89c54c679d
安第斯神鹰 <wall-e@live.cn>
2017-11-20 20:31:16 +0800

    更新安装脚本

--- install/install.sh
+++ install/install.sh
@@ -23,7 +23,7 @@ echo "
 +----------------------------------------------------------------------
 "
 #非官方安装
-github_Url=https://github.com/Vultur/bt-panel/archive
+github_Url=https://github.com/Vultur/bt-panel
 github_Tag=5.3.0
 
 #自动选择下载节点
